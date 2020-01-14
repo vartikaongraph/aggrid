211069a6154b680c5b99b7770c5d1d2c337f9c5a
vartika <vartika.ongraph@gmail.com>
2020-01-14 15:44:58 +0530

    production

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('build'));
+    app.use(express.static('/dist'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '/dist', 'index.html'));
     });
   }
   
