d2f6f4b64b241a2b02e22895bede24b2ba5766d7
vartika <vartika.ongraph@gmail.com>
2020-01-14 11:20:04 +0530

    production

--- src-examples/index.js
+++ src-examples/index.js
@@ -22,4 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#app')
     );
 });
-
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
